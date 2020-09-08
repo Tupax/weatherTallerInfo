a5b4a427af70d09c8b84a7104c6a8956f350655f
Small Potion <hello@smallpotion.co>
2020-09-07 21:31:23 -0300

    va home

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Store, select } from "@ngrx/store";
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
+import { AuthService } from "../auth.service";
 // import { Observable } from "rxjs";
 // import { environment } from "src/environments/environment";
 
@@ -10,21 +11,26 @@ import { Observable } from 'rxjs';
   styleUrls: ["./home-page.component.css"],
 })
 export class HomePageComponent implements OnInit {
-
   loc$: Observable<string>;
   loc: string;
+  loc2: string;
+
   // test = "padre";
   // test = environment.production;
-  constructor(private store: Store<any>) {
-
-    
+  constructor(private store: Store<any>, private auth: AuthService) {
     this.loc$ = store.pipe(select("loc"));
     this.loc$.subscribe((loc) => {
       this.loc = loc;
     });
   }
 
+  ngOnInit() {
+    console.log("arranco");
+    // this.auth.signup("oaksseba@gmail.com", "123456789");
+  }
 
-
-  ngOnInit() {}
+  childToParent(name) {
+    this.loc2 = name;
+    console.log(this.loc2);
+  }
 }
